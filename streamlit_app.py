1bd4b1a4228a691a23dd8a71bce9245895f1449f
kluuking <71826000+kluuking@users.noreply.github.com>
2023-04-09 18:39:23 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,16 +53,9 @@ if st.button('Press to add to CSV'):
 # Open our existing CSV file in append mode
 # Create a file object for this file
     with open('StreamLIST Sheet test.csv', 'a') as f_object:
-# Pass this file object to csv.writer()
-# and get a writer object
-  writer_object = writer(f_object)
-
- 
-# Pass the list as an argument into
-# the writerow()
-  writer_object.writerow(List)
-# Close the file object
-  f_object.close()
+        writer_object = writer(f_object)
+        writer_object.writerow(List)
+        f_object.close()
 
 else:
      st.write('Dont press unless you are sure')
