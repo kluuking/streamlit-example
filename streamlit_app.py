65281dfb6aebafaed09aad75199474d14f25ee1e
kluuking <71826000+kluuking@users.noreply.github.com>
2023-04-08 23:03:49 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,6 +34,8 @@ def load_data(sheets_url):
 
 df = load_data(st.secrets["public_gsheets_url"])
 
+print(df)
+
 # Print results.
 for row in df.itertuples():
     st.write(f"{row.name} has a :{row.pet}:")
