8be8f1828dcd0c8506cf49bf0700b543558e4348
kluuking <71826000+kluuking@users.noreply.github.com>
2023-04-08 22:54:16 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,6 +24,19 @@ st.write('You selected:', option)
 with st.echo(code_location='below'):
      Purchase_Ammount = st.slider("How many do you want to buy", 1, 10, 1) 
      st.write('You want to buy', option, Purchase_Ammount)
+     
+# Read in data from the Google Sheet.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def load_data(sheets_url):
+    csv_url = sheets_url.replace("/edit#gid=", "/export?format=csv&gid=")
+    return pd.read_csv(csv_url)
+
+df = load_data(st.secrets["public_gsheets_url"])
+
+# Print results.
+for row in df.itertuples():
+    st.write(f"{row.name} has a :{row.pet}:")
           
 with st.echo(code_location='below'):
     total_points = st.slider("Number of points in spiral", 1, 8000, 2000)
